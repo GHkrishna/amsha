fca2c73ee1539a24bedf4829c05acd4c7fe5b618
GHkrishna <kish77na@gmail.com>
2023-09-05 14:58:25 +0530

    Dynamic Cors enablement for backend server using environment variables

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const SERVER_PORT = process.env.SERVER_PORT;
+  const CLIENT_PORT_FOR_CORS = process.env.CLIENT_PORT_FOR_CORS;
+  const CLIENT_IP_FOR_CORS = process.env.CLIENT_IP_FOR_CORS;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: `${CLIENT_IP_FOR_CORS}:${CLIENT_PORT_FOR_CORS}`, // Replace with your React app's URL
+  });
+  await app.listen(SERVER_PORT);
 }
 bootstrap();
